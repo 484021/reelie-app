7138ea544d9e514ea16e0a3a2bf9c13867939c52
484021 <bhoopalsanthosh@gmail.com>
2024-08-31 19:27:03 -0400

    feat: db connection with clerk done

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/","/api/webhooks/clerk"]);
 const isProtectedRoute = createRouteMatcher(["/app/dashboard(.*)", "/app/account(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
+ 
+  
 });
 
 export const config = {

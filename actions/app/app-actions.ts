9b5f84c6f17845e1d35c42fcb4419b3acf1768e6
484021 <bhoopalsanthosh@gmail.com>
2024-08-31 21:31:07 -0400

    feat: revert

--- actions/app/app-actions.ts
+++ actions/app/app-actions.ts
@@ -6,7 +6,7 @@ import { connect } from "@/lib/db";
 export async function createUser(user: any) {
   try {
     await connect();
-    const newUser = await User.createCollection(user);
+    const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     console.log(error);
